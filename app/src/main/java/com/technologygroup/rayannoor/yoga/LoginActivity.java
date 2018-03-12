97ccd82a04f9f3c0dabaf5aed733afba8320cb09
mhm <probit.microlab@yahoo.com>
2018-03-12 08:08:30 -0700

    changes from Masdar

--- app/src/main/java/com/technologygroup/rayannoor/yoga/LoginActivity.java
+++ app/src/main/java/com/technologygroup/rayannoor/yoga/LoginActivity.java
@@ -1,20 +1,28 @@
 package com.technologygroup.rayannoor.yoga;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText edtUserName;
-    private EditText edtPass;
-    private Button btnLogin;
-    private Spinner LoginSpinner;
+
     private GradientBackgroundPainter gradientBackgroundPainter;
     private LinearLayout lytLogin;
+    private Spinner LoginSpinner;
+    private EditText edtUserName;
+    private EditText edtPass;
+    private CircularProgressButton btnLogin;
+    private TextView txtForgetPass;
+    private TextView txtRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +38,25 @@ public class LoginActivity extends AppCompatActivity {
         drawables[3] = R.drawable.gradient_4;
         gradientBackgroundPainter = new GradientBackgroundPainter(lytLogin, drawables);
         gradientBackgroundPainter.start();
+
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this , RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void initView() {
+        lytLogin = (LinearLayout) findViewById(R.id.lytLogin);
+        LoginSpinner = (Spinner) findViewById(R.id.LoginSpinner);
         edtUserName = (EditText) findViewById(R.id.edtUserName);
         edtPass = (EditText) findViewById(R.id.edtPass);
-        btnLogin = (Button) findViewById(R.id.btnLogin);
-        LoginSpinner = findViewById(R.id.LoginSpinner);
-        lytLogin = (LinearLayout) findViewById(R.id.lytLogin);
+        btnLogin = (CircularProgressButton) findViewById(R.id.btnLogin);
+        txtForgetPass = (TextView) findViewById(R.id.txtForgetPass);
+        txtRegister = (TextView) findViewById(R.id.txtRegister);
     }
 }
--- app/src/main/java/com/technologygroup/rayannoor/yoga/RegisterActivity.java
+++ app/src/main/java/com/technologygroup/rayannoor/yoga/RegisterActivity.java
@@ -1,30 +1,65 @@
 package com.technologygroup.rayannoor.yoga;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    private GradientBackgroundPainter gradientBackgroundPainter;
+    private LinearLayout lytRegister;
     private EditText edtFName;
     private EditText edtLName;
     private EditText edtMobile;
     private EditText edtEmail;
-    private Button btnRegister;
+    private EditText edtUserName;
+    private EditText edtUserPass;
+    private CircularProgressButton btnRegister;
+    private TextView txtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         initView();
+
+        //background color change
+        final int[] drawables = new int[4];
+        drawables[0] = R.drawable.gradient_1;
+        drawables[1] = R.drawable.gradient_2;
+        drawables[2] = R.drawable.gradient_3;
+        drawables[3] = R.drawable.gradient_4;
+        gradientBackgroundPainter = new GradientBackgroundPainter(lytRegister, drawables);
+        gradientBackgroundPainter.start();
+
+
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this , LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
+
     private void initView() {
+        lytRegister = (LinearLayout) findViewById(R.id.lytRegister);
         edtFName = (EditText) findViewById(R.id.edtFName);
         edtLName = (EditText) findViewById(R.id.edtLName);
         edtMobile = (EditText) findViewById(R.id.edtMobile);
         edtEmail = (EditText) findViewById(R.id.edtEmail);
-        btnRegister = (Button) findViewById(R.id.btnRegister);
+        edtUserName = (EditText) findViewById(R.id.edtUserName);
+        edtUserPass = (EditText) findViewById(R.id.edtUserPass);
+        btnRegister = (CircularProgressButton) findViewById(R.id.btnRegister);
+        txtLogin = (TextView) findViewById(R.id.txtLogin);
     }
 }
